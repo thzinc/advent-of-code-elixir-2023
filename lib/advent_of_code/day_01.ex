9d97d71e0933ee3f8e5591697032d1d05105e203
Daniel James <daniel@thzinc.com>
2023-12-01 19:09:36 -0800

    d01.p1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,5 +1,24 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(calibration_document) do
+    calibration_document
+    |> String.split("\n")
+    |> Enum.reduce(0, fn line, acc ->
+      numbers =
+        line
+        |> String.split("")
+        |> Enum.flat_map(fn str ->
+          str
+          |> Integer.parse()
+          |> case do
+            {num, _} -> [num]
+            _ -> []
+          end
+        end)
+
+      acc +
+        (List.first(numbers, 0) * 10 +
+           List.last(numbers, 0))
+    end)
   end
 
   def part2(_args) do
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,12 +3,17 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+    """
+
     result = part1(input)
 
-    assert result
+    assert result == 142
   end
 
   @tag :skip
