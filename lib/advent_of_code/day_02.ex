b880a1de380ab4f42d510c6ee57ed6f9d9c390db
Daniel James <daniel@thzinc.com>
2023-12-02 16:23:10 -0800

    d02.p1 and d02.p2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,40 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def part1(games, opts \\ []) do
+    Regex.scan(~r/(Game (?<id>\d+): (?<outcomes>.+)$)+/im, games, capture: :all_names)
+    |> Enum.reduce(0, fn [id, outcomes], acc ->
+      outcomes
+      |> String.split(";")
+      |> Enum.map(
+        &Regex.scan(~r/(?<count>\d+) (?<color>red|green|blue)/i, &1, capture: :all_names)
+      )
+      |> Enum.flat_map(& &1)
+      |> Enum.map(fn [color, count] -> {String.to_atom(color), String.to_integer(count)} end)
+      |> Enum.reduce(true, fn
+        {color, count}, acc -> if Keyword.get(opts, color, 0) < count, do: false, else: acc
+      end)
+      |> then(fn
+        true -> String.to_integer(id)
+        false -> 0
+      end)
+      |> Kernel.+(acc)
+    end)
   end
 
-  def part2(_args) do
+  def part2(games) do
+    Regex.scan(~r/(Game \d+: (?<outcomes>.+)$)+/im, games, capture: :all_names)
+    |> Enum.reduce(0, fn [outcomes], acc ->
+      outcomes
+      |> String.split(";")
+      |> Enum.map(
+        &Regex.scan(~r/(?<count>\d+) (?<color>red|green|blue)/i, &1, capture: :all_names)
+      )
+      |> Enum.flat_map(& &1)
+      |> Enum.map(fn [color, count] -> {String.to_atom(color), String.to_integer(count)} end)
+      |> Enum.reduce([red: 0, green: 0, blue: 0], fn
+        {color, count}, acc -> Keyword.put(acc, color, Kernel.max(acc[color], count))
+      end)
+      |> Enum.reduce(1, fn {_color, count}, acc -> count * acc end)
+      |> Kernel.+(acc)
+    end)
   end
 end
--- lib/mix/tasks/d02.p1.ex
+++ lib/mix/tasks/d02.p1.ex
@@ -5,13 +5,14 @@ defmodule Mix.Tasks.D02.P1 do
 
   @shortdoc "Day 02 Part 1"
   def run(args) do
-    input = nil
+    input = AdventOfCode.Input.get!(2, 2023)
+    opts = [red: 12, green: 13, blue: 14]
 
     if Enum.member?(args, "-b"),
-      do: Benchee.run(%{part_1: fn -> input |> part1() end}),
+      do: Benchee.run(%{part_1: fn -> input |> part1(opts) end}),
       else:
         input
-        |> part1()
+        |> part1(opts)
         |> IO.inspect(label: "Part 1 Results")
   end
 end
