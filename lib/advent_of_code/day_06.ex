3dd18233b538d4ac7f00c0959b9450f2ca5f927e
Daniel James <daniel@thzinc.com>
2023-12-05 21:47:47 -0800

    d06.p2 initial implementation

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -26,6 +26,28 @@ defmodule AdventOfCode.Day06 do
     |> Enum.reduce(1, &Kernel.*/2)
   end
 
-  def part2(_args) do
+  def part2(race_sheet) do
+    race_sheet
+    |> String.split("\n")
+    |> Enum.map(&String.split/1)
+    |> Enum.reduce({nil, nil}, fn
+      ["Time:" | times], {_times, distances} ->
+        {times |> Enum.join() |> String.to_integer(), distances}
+
+      ["Distance:" | distances], {times, _distances} ->
+        {times, distances |> Enum.join() |> String.to_integer()}
+
+      _, acc ->
+        acc
+    end)
+    |> then(fn {time, distance} ->
+      rate_to_beat =
+        ((time - :math.sqrt(time ** 2 - 4 * distance)) / 2)
+        |> Kernel.+(1)
+        |> trunc()
+
+      rate_to_beat..time
+      |> Enum.count(fn rate -> (time - rate) * rate > distance end)
+    end)
   end
 end
--- test/advent_of_code/day_06_test.exs
+++ test/advent_of_code/day_06_test.exs
@@ -14,11 +14,14 @@ defmodule AdventOfCode.Day06Test do
     assert result == 288
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    Time:      7  15   30
+    Distance:  9  40  200
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 71503
   end
 end
