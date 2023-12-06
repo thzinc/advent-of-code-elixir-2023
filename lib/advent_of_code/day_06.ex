411607fbf4f91e0fb945697f1b1a2a3d83760154
Daniel James <daniel@thzinc.com>
2023-12-05 21:43:10 -0800

    d06.p1 initial solution

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,5 +1,29 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(race_sheet) do
+    race_sheet
+    |> String.split("\n")
+    |> Enum.map(&String.split/1)
+    |> Enum.reduce({nil, nil}, fn
+      ["Time:" | times], {_times, distances} ->
+        {times |> Enum.map(&String.to_integer/1), distances}
+
+      ["Distance:" | distances], {times, _distances} ->
+        {times, distances |> Enum.map(&String.to_integer/1)}
+
+      _, acc ->
+        acc
+    end)
+    |> then(fn {times, distances} -> Enum.zip(times, distances) end)
+    |> Enum.map(fn {time, distance} ->
+      rate_to_beat =
+        ((time - :math.sqrt(time ** 2 - 4 * distance)) / 2)
+        |> Kernel.+(1)
+        |> trunc()
+
+      rate_to_beat..time
+      |> Enum.count(fn rate -> (time - rate) * rate > distance end)
+    end)
+    |> Enum.reduce(1, &Kernel.*/2)
   end
 
   def part2(_args) do
