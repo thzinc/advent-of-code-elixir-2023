f658160f6240170d27df16cd7b258ba8ed6b871c
Daniel James <daniel@thzinc.com>
2023-12-08 22:53:51 -0800

    d09.p2 initial solution

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -35,6 +35,41 @@ defmodule AdventOfCode.Day09 do
     end
   end
 
-  def part2(_args) do
+  def part2(report) do
+    report
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      line
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.map(fn values ->
+      values
+      |> extrapolate_first()
+      |> Enum.reverse()
+      |> Enum.reduce(&Kernel.-/2)
+    end)
+    |> Enum.reduce(&Kernel.+/2)
+  end
+
+  def extrapolate_first(seq) do
+    Enum.reduce(seq, {nil, nil, nil, []}, fn
+      value, {nil, nil, nil, []} ->
+        {value, value, value == 0, []}
+
+      value, {first, last, end?, acc} ->
+        diff = value - last
+        end? = end? and value == 0
+        {first, value, end?, [diff | acc]}
+    end)
+    |> case do
+      {first, _last, end?, revered_differences} ->
+        {first, end?, Enum.reverse(revered_differences)}
+    end
+    |> case do
+      {first, true, _differences} -> [first]
+      {first, _end?, differences} -> [first | extrapolate_first(differences)]
+    end
   end
 end
