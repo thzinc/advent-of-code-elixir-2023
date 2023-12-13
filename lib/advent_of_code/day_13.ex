275aaaeb648ee500594d1f71b8f6e5e484353e0f
Daniel James <daniel@thzinc.com>
2023-12-12 22:02:44 -0800

    d13.p2 initial solution

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -16,7 +16,8 @@ defmodule AdventOfCode.Day13 do
         column_count
       else
         row_count =
-          find_split(rows)
+          rows
+          |> find_split()
           |> case do
             nil -> 0
             count -> count
@@ -28,7 +29,34 @@ defmodule AdventOfCode.Day13 do
     |> Enum.reduce(0, &+/2)
   end
 
-  def part2(_args) do
+  def part2(notes) do
+    patterns =
+      notes
+      |> String.trim()
+      |> String.split("\n\n")
+      |> Enum.map(&String.split/1)
+
+    patterns
+    |> Enum.map(fn rows ->
+      column_count =
+        rotate(rows)
+        |> find_split(1)
+
+      if not is_nil(column_count) do
+        column_count
+      else
+        row_count =
+          rows
+          |> find_split(1)
+          |> case do
+            nil -> 0
+            count -> count
+          end
+
+        row_count * 100
+      end
+    end)
+    |> Enum.reduce(0, &+/2)
   end
 
   defp rotate(rows) do
@@ -45,24 +73,26 @@ defmodule AdventOfCode.Day13 do
     end)
   end
 
-  defp find_split(lines) do
+  defp find_split(lines, expected_errors \\ 0) do
     1..(length(lines) - 1)
     |> Enum.flat_map(fn split ->
       fore =
         lines
         |> Enum.slice(0, split)
         |> Enum.reverse()
+        |> Enum.flat_map(&String.graphemes/1)
 
       aft =
         lines
         |> Enum.slice(split, length(lines) - split)
+        |> Enum.flat_map(&String.graphemes/1)
 
-      mirrored? =
+      error_count =
         fore
         |> Enum.zip(aft)
-        |> Enum.all?(fn {f, a} -> f == a end)
+        |> Enum.count(fn {f, a} -> f != a end)
 
-      if mirrored?,
+      if error_count == expected_errors,
         do: [split],
         else: []
     end)
